533d474c733a7c04b5f7ee29a2088ec89658b3d5
Brian Seel <Brian.Seel@baltimorecity.gov>
2020-12-02 12:22:32 -0500

    Including py.typed file in wheel

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     author_email="brian.seel@baltimorecity.gov",
     description="Interface with the Ridesystems website",
     packages=['ridesystems'],
+    package_data={'ridesystems': ['py.typed'],},
     python_requires='>=3.0',
     package_dir = {'ridesystems': 'src'},
     install_requires=[
