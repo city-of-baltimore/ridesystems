5dc08447f8581232e73966776d8e6bbe48949821
Brian Seel <Brian.Seel@baltimorecity.gov>
2020-12-21 13:33:42 -0500

    Linter errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,46 +16,51 @@ def test_api_get_routes_for_map_with_schedule_with_encoded_line(ridesystems_api)
     """Test for Ridesystems API method get_routes_for_map_with_schedule_with_encoded_line"""
     ret = ridesystems_api.get_routes_for_map_with_schedule_with_encoded_line()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_map_vehicle_points(ridesystems_api):
     """Test for Ridesystems API method get_map_vehicle_points"""
     ret = ridesystems_api.get_map_vehicle_points()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_vehicle_route_stop_estimates(ridesystems_api):
     """Test for Ridesystems API method get_vehicle_route_stop_estimates"""
     ret = ridesystems_api.get_vehicle_route_stop_estimates([get_random_route(ridesystems_api)])
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_stop_arrival_times(ridesystems_api):
     """Test for Ridesystems API method get_stop_arrival_times"""
     ret = ridesystems_api.get_stop_arrival_times()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_route_stop_arrivals(ridesystems_api):
     """Test for Ridesystems API method get_route_stop_arrivals"""
     ret = ridesystems_api.get_route_stop_arrivals()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_route_schedules(ridesystems_api):
-    """Test for Ridesystems API method get_route_schedules"""
+    """
+    Test for Ridesystems API method get_route_schedules. Ride systems doesn't support this, so we don't check for data.
+    """
     ret = ridesystems_api.get_route_schedules([get_random_route(ridesystems_api)])
     assert isinstance(ret, list)
-    assert len(ret) >= 0  # Ride systems doesn't support this
+    assert len(ret) >= 0  #
 
 
 def test_api_get_route_schedule_times(ridesystems_api):
-    """Test for Ridesystems API method get_route_schedule_times"""
+    """
+    Test for Ridesystems API method get_route_schedule_times. Ride systems doesn't support this, so we don't check for
+    data
+    """
     ret = ridesystems_api.get_route_schedule_times()
     assert isinstance(ret, list)
     assert len(ret) >= 0  # Ride systems doesn't support this
@@ -65,18 +70,18 @@ def test_api_get_routes(ridesystems_api):
     """Test for Ridesystems API method get_routes"""
     ret = ridesystems_api.get_routes()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_stops(ridesystems_api):
     """Test for Ridesystems API method get_stops"""
     ret = ridesystems_api.get_stops()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_markers(ridesystems_api):
-    """Test for Ridesystems API method get_markers"""
+    """Test for Ridesystems API method get_markers. Ride systems doesn't support this, so we don't check for data."""
     ret = ridesystems_api.get_markers([get_random_route(ridesystems_api)])
     assert isinstance(ret, list)
     assert len(ret) >= 0  # Ridesystems doesn't provide this
@@ -86,23 +91,24 @@ def test_api_get_map_config(ridesystems_api):
     """Test for Ridesystems API method get_map_config"""
     ret = ridesystems_api.get_map_config()
     assert isinstance(ret, dict)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_routes_for_map(ridesystems_api):
     """Test for Ridesystems API method get_routes_for_map"""
     ret = ridesystems_api.get_routes_for_map()
     assert isinstance(ret, list)
-    assert len(ret)
+    assert ret
 
 
 def test_api_get_ridership_data(ridesystems_api):
-    """Test for Ridesystems API method get_ridership_data"""
+    """
+    Test for Ridesystems API method get_ridership_data. Ridesystems doesn't have ridership data, so we don't check for
+    data
+    """
     ret = ridesystems_api.get_ridership_data(date.today(), date.today())
     assert isinstance(ret, list)
-    assert len(ret) >= 0  # Ridesystems doesn't have ridership data
 
     yesterday = datetime.now() - timedelta(days=1)
     ret = ridesystems_api.get_ridership_data(datetime.now(), yesterday)
     assert isinstance(ret, list)
-    assert len(ret) >= 0  # Ridesystems doesn't have ridership data
