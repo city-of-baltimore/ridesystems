ce62324adcc1b0bee0c9b0058eee6aab89c542d8
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-07-12 15:27:51 -0400

    Bumping deps

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'mechanize~=0.4.5',
         'requests~=2.25.1',
-        'tenacity~=7.0.0',
+        'tenacity~=8.0.1',
         'beautifulsoup4~=4.9.3',
         'pandas~=1.3.0',
     ]
